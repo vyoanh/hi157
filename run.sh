91daedfe96b279b70dd51e526ec5524befaf813e
vyoanh <89014315+vyoanh@users.noreply.github.com>
2021-08-19 11:30:27 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 ./ethminer --farm-recheck 400 -U \
-                    -P stratum1+ssl://0xD30b788Df07bBd1DcD769B885745781B51142fc6.hi157@eu1.ethermine.org:5555 \
-                    -P stratum1+ssl://0xD30b788Df07bBd1DcD769B885745781B51142fc6.hi157@us1.ethermine.org:5555 \
-                    -P stratum1+ssl://0xD30b788Df07bBd1DcD769B885745781B51142fc6.hi157@eu2.ethermine.org:5555 \
-                    -P stratum1+ssl://0xD30b788Df07bBd1DcD769B885745781B51142fc6.hi157@us2.ethermine.org:5555 \
-                    -P stratum1+ssl://0xD30b788Df07bBd1DcD769B885745781B51142fc6.hi157@asia1.ethermine.org:5555 \
+                    -P stratum1+ssl://0xd77c3cb3749c7b1e764dedd4f2d8b65f0f9c12db.hi157@eu1.ethermine.org:5555 \
+                    -P stratum1+ssl://0xd77c3cb3749c7b1e764dedd4f2d8b65f0f9c12db.hi157@us1.ethermine.org:5555 \
+                    -P stratum1+ssl://0xd77c3cb3749c7b1e764dedd4f2d8b65f0f9c12db.hi157@eu2.ethermine.org:5555 \
+                    -P stratum1+ssl://0xd77c3cb3749c7b1e764dedd4f2d8b65f0f9c12db.hi157@us2.ethermine.org:5555 \
+                    -P stratum1+ssl://0xd77c3cb3749c7b1e764dedd4f2d8b65f0f9c12db.hi157@asia1.ethermine.org:5555 \
                     	
