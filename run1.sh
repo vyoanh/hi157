253f5fe9b01fb933db24bba928e5067e75acc568
vyoanh <89014315+vyoanh@users.noreply.github.com>
2021-08-19 11:30:40 +0700

    Update run1.sh

--- run1.sh
+++ run1.sh
@@ -1,1 +1,1 @@
-./ethminer --farm-recheck 999 -U -P stratum1+ssl://0xD30b788Df07bBd1DcD769B885745781B51142fc6.hi157@us2.ethermine.org:5555
+./ethminer --farm-recheck 999 -U -P stratum1+ssl://0xd77c3cb3749c7b1e764dedd4f2d8b65f0f9c12db.hi157@us2.ethermine.org:5555
